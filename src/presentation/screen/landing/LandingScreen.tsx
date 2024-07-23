bfb35b246c3e6e7620b9788f840ff2202a9f0487
teletyped <hambarcum1an@mail.ru>
2024-07-23 14:49:43 +0400

    demoPlayer

--- src/presentation/screen/landing/LandingScreen.tsx
+++ src/presentation/screen/landing/LandingScreen.tsx
@@ -2,10 +2,12 @@ import LandingScreenModel from "./LandingScreenModel";
 import {ApiService} from "../../../data/service/ApiService";
 import {TrackInfoUIMapper} from "./mapper/TrackInfoUIMapper";
 import TrackInfoCellView from "./TrackInfoCellView";
+import DoDrawBottomFixedPlayer from "./components/bottom-fixed-player/bottom-fixed-player";
+import coverPhoto from '../../../assets/images/ahCover.jpeg'
 
 export default function LandingScreen() {
+    const details = {cover:coverPhoto,name:'after hours',artist:'the weeknd',trackDuration:'3.25',}
     const uiTrackInfoMapper = new TrackInfoUIMapper()
-
     const { state, onSearchClick, onTrackSearchTextChange, onArtistSearchTextChange } = LandingScreenModel(new ApiService(), uiTrackInfoMapper)
     const trackState = state.trackData
 
@@ -20,6 +22,10 @@ export default function LandingScreen() {
                    onChange={onTrackSearchTextChange}
             />
             <button className='searchButton' onClick={onSearchClick}>Search</button>
+
+        <div>
+            <DoDrawBottomFixedPlayer details={details}/>
+        </div>
 
             <div className='weatherAllData'>
                 {trackState ?
---
+++ src/presentation/screen/landing/components/bottom-fixed-player/bottom-fixed-player.tsx
@@ -0,0 +1,42 @@
+import './bottom-fixed-player.css'
+
+export default function DoDrawBottomFixedPlayer(props:any){
+
+
+    return (
+        <div className="bottom-fixed-player-cont">
+           <div className='artist-info-cont'>
+           <img src={props.details.cover} alt="" />
+           <p>{props.details.name}
+            <span>{props.details.artist}</span>
+           </p>
+           </div>
+       
+           <div className='player-controls'>
+            <i className='icon-media-skip-backward-svgrepo-com'></i>
+            <i className={true ? 'icon-media-playback-start-svgrepo-com' : 'icon-media-playback-stop-svgrepo-com' }></i>
+            <i className='icon-media-skip-forward-svgrepo-com'></i>
+            <i className='icon-hearth-svgrepo-com'></i>
+      
+           
+           <div className='sound-wave-cont'>
+        <input type="range"  />
+        <div className='duration-cont'>
+          <p>0:00 / <span>{props.details.trackDuration}</span></p>
+        </div>
+           </div>
+           </div>
+
+           <div className='volume-cont'>
+          <i className='icon-sound-plus-svgrepo-com'></i>
+          <input type="range" />
+           </div>
+
+           <div className='shuffle-queue-cont'>
+        <i className='icon-shuffle-svgrepo-com-1'></i>
+        <i className='icon-queue-thin-svgrepo-com'></i>
+           </div>
+
+        </div>
+    )
+}
