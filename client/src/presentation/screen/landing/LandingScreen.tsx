681b48877afa9c6e2403d3f9c16d21736efdd05b
InnaMnatsakanyan <inna.mnacakanyan1@gmail.com>
2024-08-13 01:38:15 +0400

    Loader screen added

--- client/src/presentation/screen/landing/LandingScreen.tsx
+++ client/src/presentation/screen/landing/LandingScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "../../components/Header";
 import LandingScreenModel from "./LandingScreenModel";
 import { ApiService } from "../../../data/service/ApiService";
@@ -7,27 +7,52 @@ import { TopArtistUIMapper } from "../mappers/TopArtistUIMapper";
 import styles from './LandingScreen.module.css';
 import TopArtistCellView from "./TopArtistCellView";
 import TopTracksCellView from "./TopTracksCellView";
+import Loader from "../loader/Loader";
 import {TopTracksUIMapper} from "../mappers/TopTracksUIMapper";
 
 export default function LandingScreen() {
     const uiTopTracksMapper = new TopTracksUIMapper();
     const uiTopArtistMapper = new TopArtistUIMapper();
 
-    const { state, onTrackSearchTextChange, onTrackSearchClick, onKeyPress } = LandingScreenModel(new ApiService(), uiTopArtistMapper, uiTopTracksMapper);
+    const {
+        state,
+        onTrackSearchTextChange,
+        onTrackSearchClick,
+        onKeyPress
+    } = LandingScreenModel(new ApiService(), uiTopArtistMapper, uiTopTracksMapper);
 
     const topTracksState = state.topTracksData;
     const topArtistState = state.topArtistData;
 
     console.log(state.topTracksData?.tracks[2].name)
     console.log(state.topArtistData?.name.toString())
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                await new Promise((resolve) => setTimeout(resolve, 2000));
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
-        <div>
-            <Header
-                onTrackSearchClick={onTrackSearchClick}
-                onTrackSearchTextChange={onTrackSearchTextChange}
-                onKeyPress={onKeyPress}
-            />
+        <div className="landing-screen">
+            {isLoading ? (
+                <div className={styles.centerDivHelperClass}>
+                    <Loader/>
+                </div>
+            ) : (
+                <div>
+                    <Header
+                        onTrackSearchClick={onTrackSearchClick}
+                        onTrackSearchTextChange={onTrackSearchTextChange}
+                        onKeyPress={onKeyPress}
+                    />
 
             <div className={styles.topPickCells}>
                 <div className={styles.topPickCellsIn}>
@@ -40,24 +65,26 @@ export default function LandingScreen() {
                 </div>
             </div>
 
-            <div className={styles.similarTracksComponent}>
-                {topTracksState ?
-                    <p className={styles.resultText}>Trending Now</p>
-                    : null
-                }
-                <div className={styles.similarTracksTotal}>
-                    {topTracksState ? topTracksState.tracks.slice(0, 20).map((track, index) => (
-                        <TopTracksCellView
-                            key={track.mbid || index}
-                            name={track.name}
-                            artist={track.artist.name}
-                            mbid={track.mbid}
-                            duration={track.duration}
-                        />
-                    )) : null
-                    }
+                    <div className={styles.similarTracksComponent}>
+                        {topTracksState ?
+                            <p className={styles.resultText}>Trending Now</p>
+                            : null
+                        }
+                        <div className={styles.similarTracksTotal}>
+                            {topTracksState ? topTracksState.tracks.slice(0, 20).map((track, index) => (
+                                <TopTracksCellView
+                                    key={track.mbid || index}
+                                    name={track.name}
+                                    artist={track.artist.name}
+                                    mbid={track.mbid}
+                                    duration={track.duration}
+                                />
+                            )) : null
+                            }
+                        </div>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 }
